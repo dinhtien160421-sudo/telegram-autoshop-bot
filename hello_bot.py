ffdcd34ae393eba81e03d6ef4d3f89d8b7b7dc93
dinhtien160421-sudo <dinhtien160421@gmail.com>
2026-01-11 07:58:04 +0700

    Update price for veo3_ultra_30d

--- hello_bot.py
+++ hello_bot.py
@@ -58,7 +58,7 @@ PRODUCTS = {
     },
     "veo3_ultra_30d": {
         "name": "VEO3 ULTRA 45K CREDIT BH 30D",
-        "price": 50000,
+        "price": 45678,
     },
     "gpt_plus_30d": {
         "name": "ACC GPT PLUS 30D",
