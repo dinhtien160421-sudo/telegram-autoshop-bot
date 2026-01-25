46203888c2fea6281dd523fd6dd56834b46bc788
dinhtien160421-sudo <dinhtien160421@gmail.com>
2026-01-25 13:34:52 +0700

    Update hello_bot.py

--- hello_bot.py
+++ hello_bot.py
@@ -50,24 +50,23 @@ def add_user(chat_id: int):
 # Danh sách sản phẩm
 PRODUCTS = {
     "code_gpt": {
-        "name": "CODE GPT PLUS",
+        "name": "CODE GPT PLUS\n(còn {stock})",
         "price": 15000,
     },
     "veo3_ultra_bh": {
-        "name": "VEO3 ULTRA 45K CRE BH 4/2",
-        "price": 75000 ,
+        "name": "VEO3 ULTRA 45K CREDIT\nBH ĐẾN 4/2\n(còn {stock})",
+        "price": 75000,
     },
     "veo3_ultra_bhf": {
-        "name": "VEO3 ULTRA 45K CRE BH 30D",
+        "name": "VEO3 ULTRA 45K CREDIT\nBH 30 NGÀY\n(còn {stock})",
         "price": 130000,
     },
-
     "info_1": {
-        "name": "Gia hạn GPT Plus – Capcut - Canva - Inbox",
+        "name": "Gia hạn GPT Plus – Capcut – Canva\nInbox",
         "price": 0,
     },
     "info_2": {
-        "name": "Zalo: 0842.108.959 - Tele:@dtdt28",
+        "name": "Zalo: 0842.108.959\nTele: @dtdt28",
         "price": 0,
     },
 }
