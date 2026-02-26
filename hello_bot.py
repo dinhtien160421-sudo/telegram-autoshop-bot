2cc54307fa2a9ef18707c7378ed242ceea43269c
dinhtien160421-sudo <dinhtien160421@gmail.com>
2026-02-26 09:31:52 +0700

    Update hello_bot.py

--- hello_bot.py
+++ hello_bot.py
@@ -32,7 +32,7 @@ PENDING_ORDERS = {}
 
 # ===== SẢN PHẨM =====
 PRODUCTS = {
-    "veo3_pro_bh": {"name": "Veo3 Pro 45K cre BH 24H", "price": 60000},
+    "veo3_pro_bh": {"name": "Veo3 Pro 45K cre BH 24H", "price": 55000},
     "canva_pro_1m": {"name": "Canva Pro 30D BHF", "price": 25000},
     "canva_pro_6m": {"name": "Canva Pro 6 THÁNG BHF", "price": 100000},
     "info_2": {"name": "Capcut Pro Team 30-35D BHF", "price": 18000},
@@ -57,10 +57,6 @@ STOCK = {
 "zed84k7ns@k.turrubiates.pro.vn|ThongChoLon@999",
 "zed87rl1x@k.turrubiates.pro.vn|ThongChoLon@999",
 "zedacooja@k.turrubiates.pro.vn|ThongChoLon@999",
-"zedbxlv3p@k.turrubiates.pro.vn|ThongChoLon@999",
-"zedawy167@k.turrubiates.pro.vn|ThongChoLon@999",
-"zedbaxml7@k.turrubiates.pro.vn|ThongChoLon@999",
-
 
     ],
     
