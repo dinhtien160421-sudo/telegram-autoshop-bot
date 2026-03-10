dd9e3aaf7f028cb0883a0ab86138a2fad545f435
dinhtien160421-sudo <dinhtien160421@gmail.com>
2026-03-10 14:08:28 +0700

    Update hello_bot.py

--- hello_bot.py
+++ hello_bot.py
@@ -32,7 +32,7 @@ PENDING_ORDERS = {}
 
 # ===== SẢN PHẨM =====
 PRODUCTS = {
-    "veo3_ultra_bh": {"name": "Veo3 Ultra 45K cre BH 24H", "price":49999},
+    "veo3_ultra_bh": {"name": "Veo3 Ultra 45K cre BH 24H", "price":45678},
     "veo3_pro_bh": {"name": "Veo3 Pro 45K cre BH 24h", "price": 22222},
     "veo3_0k_bhf_1m": {"name": "Veo3 Ultra 0 credit chính chủ BHF 1 tháng", "price": 290000},
     "veo3_ultra_bhf_1m": {"name": "Veo3 Ultra 5k credit chính chủ BHF 1 tháng", "price": 350000},
@@ -86,8 +86,6 @@ STOCK = {
 
     ],
     "veo3_pro_bh":[    
-"ZedNanR@tunqerostaviona.sbs|ThongChoLon@999",
-"ZedPmoq@tunqerostaviona.sbs|ThongChoLon@999",
 "ZedQzUZ@tunqerostaviona.sbs|ThongChoLon@999",
 "ZedSxPa@tunqerostaviona.sbs|ThongChoLon@999",
 "ZedTStK@tunqerostaviona.sbs|ThongChoLon@999",
