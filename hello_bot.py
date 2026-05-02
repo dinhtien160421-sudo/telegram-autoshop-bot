b4e143af23ed335eef91a7be09bd26e56c2cb121
dinhtien160421-sudo <dinhtien160421@gmail.com>
2026-05-02 21:08:26 +0700

    Update hello_bot.py

--- hello_bot.py
+++ hello_bot.py
@@ -35,7 +35,7 @@ PRODUCTS = {
     "veo3_ultra_bh": {"name": "Veo3 Ultra 25K cre BH 24H", "price":25555},
     "Grok": {"name": "Veo3 Ultra 25K cre BH 30D", "price":200000},
     "Capcut_Pro": {"name": "Capcut Pro Team 35D Renew", "price":15000},
-    "Fam_Ultra": {"name": "Fam Google Ultra Add 5 slot BH 3D", "price:750000":},
+    "Fam_Ultra": {"name": "Fam Google Ultra Add 5 slot BH 3D", "price":750000":},
     "Fam_Ultra_bhf": {"name": "Fam Google Ultra Add 5 slot BHF 30D", "price":1500000},
     "veo3_0k_bhf_1m": {"name": "Ultra Add Fam 0 credit BHF 1 tháng", "price": 290000},
     "veo3_ultra_bhf_1m": {"name": "Ultra Add Fam 5k credit BHF 1 tháng", "price": 330000},
